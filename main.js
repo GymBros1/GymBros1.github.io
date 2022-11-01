4f126f83a73c2b75b9cefb6cc6882b0fffa05b4f
Angus Leung <angusleung168@gmail.com>
2022-10-31 23:47:08 -0400

    Moved around some existing code

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-var a = 1;
+function OpenPopupWindow()
+{
+    var url = "pop.html";
+    myRef = window.open(url ,'mywin','left=20,top=20,width=1000,height=50,toolbar=1,resizable=0');
+    myRef.focus();
+}
